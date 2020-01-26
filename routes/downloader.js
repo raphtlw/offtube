2ffbd5878621893b7eaf4fdb87929840eb852832
Raphael Tang <raphpb1912@gmail.com>
2020-01-27 00:31:45 +0800

    Better error handling 🧪 Added new page when error occurs 🧪 Code cleanup 🧹 Videos now download with video title as the name 🔧

--- routes/downloader.js
+++ routes/downloader.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 const router = express.Router();
+
+// TODO: Download at best formats by default
+// TODO: Convert from any format to mp4 if possible
 
 // Routes
 
 router.get('/', (req, res) => {
   const url = req.query.url
-  console.log(url);
+  console.log(url);  // For testing
+
+  ytdl.getBasicInfo(url).then(
+    (info) => {
+      console.log(`Title: ${info.title}`);
+      console.log(`Author: ${info.author.name}`);
+      return info;
+    }
+  ).then((info) => {
+    res.header('Content-Disposition', `attachment; filename="${info.title}"`);
+    ytdl(
+      url,
+      { format: 'mp4' }
+    ).pipe(res);
+  });
 });
 
 module.exports = router;
