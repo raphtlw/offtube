b042355b824ae59292689aac9b80178eb0fe76f2
Raphael Tang <raphpb1912@gmail.com>
2020-01-26 21:11:16 +0800

    Fixed bugs 🚀

--- routes/downloader.js
+++ routes/downloader.js
@@ -1,42 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const youtubedl = require('youtube-dl');
 
 const router = express.Router();
 
 // Routes
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'url_error.html'));
-});
-
-// :url is a request parameter e.g: (http://localhost:5000/index.html/send-url/https://example.com)
-router.get('/:url', (req, res) => {
-  const url = req.params.url
-
-  const video = youtubedl(url);
-  video.on('info', info => {
-    console.log('Download started');
-    console.log(`title: ${info.title}`);
-    console.log(`url: ${info.url}`);
-    console.log('-----------------------------');
-    console.log(`filename: ${info._filename}`);
-    console.log(`size: ${info.size}`);
-    console.log(`format id: ${info.format_id}`);
-  });
-  
-  video.pipe(fs.createWriteStream('video.mp4'));
-
-  const videoFilePath = path.join(__dirname, 'video.mp4');
-
-  res.download(videoFilePath, (err) => {
-    if (!err) {
-      fs.unlink(videoFilePath);
-    } else {
-      console.error(err);
-    }
-  });
+  const url = req.query.url
+  console.log(url);
 });
 
 module.exports = router;
