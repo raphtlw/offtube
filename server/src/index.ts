fe3e5ef7b175c3ab3eaf214e3b4b8222b3144937
Raphael Tang <raphpb1912@gmail.com>
2020-04-04 19:37:33 +0800

    Added best quality thing

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,30 +13,22 @@ app.get('/', (req, res) => {
 app.get('/download/video', (req, res) => {
   const { url } = req.query;
   console.log(`URL: ${url}`);
-  // const video = youtubedl(url, [], {});
-
-  // video.on('info', info => {
-  //   console.log('Download started');
-  //   console.log(`Filename: ${info._filename}`);
-  //   console.log(`Size: ${info.size}`);
-
-  //   video.pipe(fs.createWriteStream(info._filename));
-  //   video.on('end', () => {
-  //     res.download(info._filename, err => fs.unlinkSync(info._filename));
-  //   });
-  // });
-
-  youtubedl.exec(
+  const video = youtubedl(
     url,
     ['-f', 'bestvideo[ext=mp4]+bestaudio[ext=m4a]'],
-    {},
-    (err, output) => {
-      if (err) throw err;
-      console.log(output.join('\n'));
-    }
+    {}
   );
 
-  res.send('testing');
+  video.on('info', info => {
+    console.log('Download started');
+    console.log(`Filename: ${info._filename}`);
+    console.log(`Size: ${info.size}`);
+
+    video.pipe(fs.createWriteStream(info._filename));
+    video.on('end', () => {
+      res.download(info._filename, err => fs.unlinkSync(info._filename));
+    });
+  });
 });
 
 app.get('/download/audio', (req, res) => {});
